9b16bdc344d6bc0c6dcac207bb05f6e644380adb
Alexson Almeida <as3904527@gmail.com>
2025-01-10 11:37:43 -0300

    adding the useState

--- src/RepositorieList.jsx
+++ src/RepositorieList.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import {Repositorie} from './Repositorie'
+import React, { useState } from 'react';
+import { Repositorie } from './Repositorie';
+import { Plus } from 'lucide-react';
 
 export function RepositorieList(props) {
-  const repositoriesList = [
+  const [repositoriesList, setRepositoriesList] = useState([
     {
       name: 'Swift',
       link: 'https://github.com/swiftlang.png',
@@ -18,7 +19,8 @@ export function RepositorieList(props) {
       link: 'https://github.com/tailwindlabs.png',
       repositoryLink: 'https://github.com/tailwindlabs/tailwindcss'
     }
-  ]
+  ])
+  const [counterState, setCounterState] = useState(repositoriesList.length)
   return (
     <div className=''>
       {repositoriesList.map(repository => <Repositorie link={repository.link} name={repository.name} repositoryLink={repository.repositoryLink}/>)}
@@ -27,6 +29,11 @@ export function RepositorieList(props) {
       :
         <h4>I'm a repository enthusiast</h4>
       }
+      <button className='button' onClick={() => setCounterState(counterState+1)}>
+        Add repository <Plus />
+      </button>
+
+      <h4>Counting of repository: {counterState}</h4>
     </div>
   );
 }
